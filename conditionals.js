05afa9fc26fffe71194ad8235603e529064bdc1a
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 12:32:18 -0500

    add conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+// Question 1: Write an if-else statement that checks whether a variable num is
+// positive, negative, or zero. Print the appropriate message accordingly.
+
+
+// Question 2: Write a JavaScript function called getDiscount that takes a purchase
+// amount as an argument. If the purchase amount is greater than $100, apply a 10%
+// discount. If it is less than or equal to $100, apply a 5% discount. Return the
+// discounted amount.
+
+
+// Question 3: Write an if-else statement that checks whether a year is a leap year.
+// Print "Leap year" if it is divisible by 4 but not divisible by 100, or if it is divisible
+// by 400. Otherwise, print "Not a leap year".
