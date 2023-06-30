ab5298cf527f2b47bdb8a954aa959087fd902632
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 12:22:09 -0500

    add math.js

---
+++ math.js
@@ -0,0 +1,12 @@
+// Question 1: Generate a random number between 1 and 100 (inclusive) using the
+// Math.random() function and round it to the nearest integer. Store the result in a
+// variable called randomNumber.
+
+
+// Question 2: Write a JavaScript function called calculateFactorial that takes a
+// number as an argument and returns its factorial.
+
+
+// Question 3: Write a JavaScript function called calculatePower that takes two
+// numbers, base and exponent, as arguments and returns the result of base raised to
+// the power of exponent.
