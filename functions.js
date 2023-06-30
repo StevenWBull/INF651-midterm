112614413d2a29e08f436b9a0a0c1d146666a795
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 12:46:26 -0500

    finish loop.js, add functions.js file

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Question 1: Write a JavaScript function called reverseString that takes a string as
+// an argument and returns the reversed version of the string.
+
+
+// Question 2: Write a JavaScript function called calculateBMI that takes a person's
+// weight (in kilograms) and height (in meters) as arguments and returns their body
+// mass index (BMI).
+
+
+// Question 3: Write a JavaScript function called capitalizeWords that takes a
+// sentence as an argument and returns the sentence with each word capitalized.
---
+++ loops.js
@@ -0,0 +1,40 @@
+// Question 1: Write a while loop that prints the numbers from 1 to 20.
+
+let i = 1;
+while (i <= 20) {
+    console.log(i);
+    i++;
+};
+
+// o Question 2: Write a JavaScript function called calculateSum that takes a positive
+// integer n as an argument and returns the sum of all numbers from 1 to n.
+
+const calculateSum = (n) => {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    };
+    return sum;
+};
+
+console.log(`calculateSum(1) should be 1: ${calculateSum(1)}`);
+console.log(`calculateSum(2) should be 3: ${calculateSum(2)}`);
+console.log(`calculateSum(3) should be 6: ${calculateSum(3)}`);
+
+// o Question 3: Write a JavaScript function called calculateFactorialIterative that
+// takes a positive integer n as an argument and calculates its factorial using a loop
+// instead of recursion.
+
+const calculateFactorialIterative = (n) => {
+    let product = 1;
+    for (let i = 1; i <= n; i++) {
+        product *= i;
+    };
+    return product;
+};
+
+console.log(`calculateFactorialIterative(0) should be 1: ${calculateFactorialIterative(0)}`);
+console.log(`calculateFactorialIterative(1) should be 1: ${calculateFactorialIterative(1)}`);
+console.log(`calculateFactorialIterative(2) should be 2: ${calculateFactorialIterative(2)}`);
+console.log(`calculateFactorialIterative(3) should be 6: ${calculateFactorialIterative(3)}`);
+console.log(`calculateFactorialIterative(4) should be 24: ${calculateFactorialIterative(4)}`);
