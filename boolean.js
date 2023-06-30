e3b57038da80e1b313c555a1b2b6af2d586a30ff
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 11:48:40 -0500

    add the boolean.js file

---
+++ boolean.js
@@ -0,0 +1,12 @@
+// Question 1: Write a condition that checks whether a variable isRaining is true and
+// a variable isSunny is false. Store the result in a variable called shouldStayInside.
+
+
+// Question 2: Write a JavaScript function called hasPermission that takes two
+// boolean parameters, isUserLoggedIn and isAdmin, and returns true if the user has
+// permission and false otherwise.
+
+
+// Question 3: Write a JavaScript function called hasUniqueCharacters that takes a
+// string as an argument and returns true if all characters in the string are unique and
+// false otherwise.
