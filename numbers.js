a5ff6cd98f25623aa58b290a8fbe804addbef609
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 11:23:56 -0500

    add numbers.js

---
+++ numbers.js
@@ -0,0 +1,11 @@
+// Question 1: Calculate the square root of a given number and store the result in a
+// variable called result.
+
+
+// Question 2: Write a JavaScript function called isPrime that takes a number as an
+// argument and returns true if it is a prime number and false otherwise.
+
+
+// Question 3: Write a JavaScript function called generateRandomNumber that takes
+// two numbers, min, and max, as arguments and returns a random number between
+// min and max
