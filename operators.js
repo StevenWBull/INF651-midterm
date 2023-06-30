b5634c0ae0919e10bf7079bcd37713a86947394c
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 11:36:18 -0500

    create operators.js file

---
+++ operators.js
@@ -0,0 +1,11 @@
+// Question 1: Given the variables a = 10 and b = 3, what is the value of the
+// following expression: a % b?
+
+
+// Question 2: Write a JavaScript function called isInRange that takes a number as
+// an argument and returns true if it falls within a specific range (e.g., between 10
+// and 20) and false otherwise.
+
+
+// Question 3: Given the variables a = 5, b = 3, and c = 2, what is the value of the
+// following expression: a > b || c < b && a === c?
