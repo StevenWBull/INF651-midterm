3234e7ab0bc2b8479eec1a79338f8765a0cc2b70
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-06-30 11:22:54 -0500

    finish variables.js section

--- variables.js
+++ variables.js
@@ -2,6 +2,21 @@
 // choice. Then, display a message that says "Hello, [name]!" using the value of the
 // name variable.
 
+let name = 'Steven';
+
+console.log(`Hello, ${name}!`);
+// or in browser alert(`Hello, ${name}!`);
 
 // Question 2: Create two variables, x, and y, and assign them numerical values.
 // Swap the x and y values using a third variable and display the updated values.
+
+let x = 5;
+let y = 10;
+
+console.log(`Before swap x is ${x} and y is ${y}`);
+
+let temp = x;
+x = y;
+y = temp;
+
+console.log(`After swap x is ${x} and y is ${y}`);
